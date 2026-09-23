cd67d07da6011ff1a2f8a82c69a27ddcee02a10c
ENG-Adel <adelelgyoushy61@gmail.com>
2026-09-23 22:52:28 +0300

    Update button text for message and link copying

--- js/diplomas.js
+++ js/diplomas.js
@@ -39,10 +39,10 @@
         </ul>
         <div class="dip-actions">
           <button class="btn-eh" type="button" data-act="copy-msg" data-id="${d.id}">
-            <i class="bi bi-clipboard2"></i><span>نسخ الرسالة</span>
+            <i class="bi bi-clipboard2"></i><span> نسخ الرسالة كامله </span>
           </button>
           <button class="btn-eh btn-eh--ghost" type="button" data-act="copy-link" data-id="${d.id}">
-            <i class="bi bi-link-45deg"></i><span>نسخ اللينك</span>
+            <i class="bi bi-link-45deg"></i><span> نسخ اللينك فقط </span>
           </button>
         </div>
         <div class="dip-foot">
