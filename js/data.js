0c332d9847889a3067dea8723d6988b65e65d553
ENG-Adel <adelelgyoushy61@gmail.com>
2026-09-26 01:14:14 +0300

    Update Facebook group link and description

--- js/data.js
+++ js/data.js
@@ -470,7 +470,7 @@ window.SOCIAL_MEDIA_TEXT = `Eraa-social media
 🔹 فيسبوك:
 📘 https://facebook.com/eraasoft
 🔹 جروب الفيسبوك (مجتمع المتعلمين):
-👥 https://facebook.com/groups/2128494490777624
+👥 https://www.facebook.com/share/g/1AsLEkxjXx/
 🔹 انستجرام:
 📸 https://instagram.com/eraasoft1
 🔹 قناة واتساب:
@@ -485,7 +485,7 @@ window.SOCIAL_MEDIA_TEXT = `Eraa-social media
 window.SOCIAL_MEDIA_LINKS = [
   { name: "الموقع الرسمي", url: "https://www.eraasoft.com", icon: "bi-globe", color: "#0d6efd" },
   { name: "صفحة فيسبوك", url: "https://facebook.com/eraasoft", icon: "bi-facebook", color: "#1877f2" },
-  { name: "جروب الفيسبوك (مجتمع المتعلمين)", url: "https://facebook.com/groups/2128494490777624", icon: "bi-people-fill", color: "#1877f2" },
+  { name: "جروب الفيسبوك (Community)", url: "https://www.facebook.com/share/g/1AsLEkxjXx/", icon: "bi-people-fill", color: "#1877f2" },
   { name: "انستجرام", url: "https://www.instagram.com/eraa_soft?stkn=MXJvb284aXNicDc4Nw==", icon: "bi-instagram", color: "#e4405f" },
   { name: "قناة واتساب", url: "https://whatsapp.com/channel/0029VatAmJmBadmhpf2zZb47", icon: "bi-whatsapp", color: "#25d366" },
   { name: "تليجرام", url: "https://t.me/et3lembarmagah", icon: "bi-telegram", color: "#24a1de" },
